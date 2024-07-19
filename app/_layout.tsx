c3686bfd157e7cbd93fa54a042770c59916f9476
terrence.gonsalves <terrence.gonsalves@gmail.com>
2024-07-18 23:05:05 -0600

    updated default colours

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
