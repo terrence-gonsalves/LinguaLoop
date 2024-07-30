074fe5ed75a08adce98d4500f4f3e75a8b40be89
terrence.gonsalves <terrence.gonsalves@gmail.com>
2024-07-30 12:36:45 -0600

    testing some layouts for the dashboard

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -19,6 +19,30 @@ export const Colors = {
     link: '#1cb0f6',
     generalBG: '#F7F8FB',
     tabsBG: '#6E8CA0',
+    
+    // typography
+    textPrimary: '#1B1C20',
+    textSecondary: '#6E8CA0',
+    textTertiary: '#FFFFFF',
+
+    formInputBG: '#F7F8FB',
+    formInputBorder: '#E4E6ED',
+    
+    // Misc
+    borders: '#F8F4F9',
+    grey: '#e8e8e8',
+    black: '#050401',
+    appBG: '#eceff0',
+
+    // primary colours
+    sage: '#87BCBF',
+    rust: '#D97D54',
+    drab: '#324755',
+
+     // secondary colours
+     ice: '#F0F3F4',
+     fossil: '#C8D1D3',
+     sand: '#B9B0A2',
   },
   dark: {
     text: '#ECEDEE',
