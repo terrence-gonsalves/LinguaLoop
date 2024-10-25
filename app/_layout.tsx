391ca1959689c3805e6d8347de6c6b63be40470e
terrence.gonsalves <terrence.gonsalves@gmail.com>
2024-10-25 10:57:00 -0600

    removed some dependancies added gluestick  UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,27 @@
 import { Stack } from "expo-router";
+import "'@/global.css'";
+import { GluestackUIProvider } from "@/'components/ui'/gluestack-ui-provider";
+import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <StatusBar style="auto" />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GluestackUIProvider mode="light"><ThemeProvider value={DefaultTheme}>
+        <StatusBar style="auto" />
+        <SafeAreaView style={styles.wrapper}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </SafeAreaView>
+      </ThemeProvider></GluestackUIProvider>
   );
 }
+
+const styles = StyleSheet.create({  
+  wrapper: {
+    flex: 1,
+  },
+});
