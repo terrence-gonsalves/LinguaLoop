9ec847a8668232de0034763af6d8fae41619fcff
terrence.gonsalves <terrence.gonsalves@gmail.com>
2024-10-26 20:33:11 -0600

    testing a badge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { TamaguiProvider } from 'tamagui';
-import { useColorScheme } from "@/components/useColorScheme";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TamaguiProvider, createTamagui } from 'tamagui';
+import { config } from '@tamagui/config/v3'
+
+const tamaguiConfig = createTamagui(config);
+
+type Conf = typeof tamaguiConfig;
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+};
+
 
 export default function RootLayout() {
-
   return (
-    <TamaguiProvider>
+    <TamaguiProvider config={tamaguiConfig}>
       <StatusBar style="auto" />
       <SafeAreaView style={styles.wrapper}>
         <Stack>
