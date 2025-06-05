0bbfd8f730b8983c1b984807e802e5ae1184e30a
terrence.gonsalves <terrence.gonsalves@gmail.com>
2025-06-05 12:05:15 -0600

    Fixed uncaught error and loading settings screen properly

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -4,7 +4,7 @@ export default function SettingsLayout() {
   return (
     <Stack>
       <Stack.Screen 
-        name="test" 
+        name="index" 
         options={{
           headerShown: false,
         }}
